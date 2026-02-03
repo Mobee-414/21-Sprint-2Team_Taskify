44f9acfee27f7fc4a6ccab8d81e423c24d731cb7
joara-thrlaw <arjoe207@gmail.com>
2026-02-03 18:12:18 +0900

    🚚 chore: 프로젝트 초기 환경 세팅 및 디자인 시스템 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-// import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+import "@/styles/globals.css";
+
+const pretendard = localFont({
+  src: [
+    { path: "../../public/fonts/Pretendard-Bold.woff2", weight: "700" },
+    { path: "../../public/fonts/Pretendard-ExtraBold.woff2", weight: "600" },
+    { path: "../../public/fonts/Pretendard-Medium.woff2", weight: "500" },
+    { path: "../../public/fonts/Pretendard-Regular.woff2", weight: "400" },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={pretendard.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
