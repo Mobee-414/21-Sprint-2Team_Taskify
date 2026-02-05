d4d7d9afac67612171e55e1cd0a489d826d02aed
joara-thrlaw <arjoe207@gmail.com>
2026-02-05 13:57:15 +0900

    🎨 style: CardDetailModal 컴포넌트 피드백 반영(dl 태그 추가)

--- src/components/modals/CardDetailModal/SideInfo.tsx
+++ src/components/modals/CardDetailModal/SideInfo.tsx
@@ -2,17 +2,21 @@ export default function SideInfo() {
   return (
     <ul>
       <li>
-        <dt>담당자</dt>
-        <dd>
-          <div>
-            <div>담당자 프로필 이미지</div>
-            담당자 닉네임
-          </div>
-        </dd>
+        <dl>
+          <dt>담당자</dt>
+          <dd>
+            <div>
+              <div>담당자 프로필 이미지</div>
+              담당자 닉네임
+            </div>
+          </dd>
+        </dl>
       </li>
       <li>
-        <dt>마감일</dt>
-        <dd>0000.00.00 00:00</dd>
+        <dl>
+          <dt>마감일</dt>
+          <dd>0000.00.00 00:00</dd>
+        </dl>
       </li>
     </ul>
   );
