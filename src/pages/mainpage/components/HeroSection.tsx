24ef0c726cae1985c1e017e1f4250ad4f08ae7dd
ingyu <ingyu@bag-ingyuui-MacBookPro.local>
2026-02-13 09:49:45 +0900

    🐛 HeroSction 컨플릭트 수정

--- src/pages/mainpage/components/HeroSection.tsx
+++ src/pages/mainpage/components/HeroSection.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { login } from "@/api/auth.api";
+import { signup, login } from "@/api/auth.api";
 import { AxiosError } from "axios";
 
 const HeroSection = () => {
